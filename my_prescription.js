bcad6fefca43cf943e0811065a0b1d9c293e3794
blendosmani73 <bo73874@ubt-uni.com>
2025-12-17 18:22:50 +0100

    Shtimi i validimit tek my prescription dhe ne klikim te ok te dergohet tek the doctor's report

---
+++ my_prescription.js
@@ -0,0 +1,23 @@
+const idInput = document.getElementById("id-number");
+const idError = document.getElementById("idError");
+const btn2 = document.getElementById("button2");
+
+const idRegex = /^\d{10}$/;  
+
+btn2.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    if (idInput.value.trim() === "") {
+        idError.textContent = "ID number is required";
+        idError.style.visibility = "visible";
+    } else if (!idRegex.test(idInput.value.trim())) {
+        idError.textContent = "ID must be exactly 10 digits ";
+        idError.style.visibility = "visible";
+    } else {
+        idError.style.visibility = "hidden";
+        alert("ID number accepted!");
+        window.location.href = "the_doctor's_report.html";
+    }
+});
+
+
